016d62b1ad45f31c34015d59f0a9589f6291a348
Khaled <khaledonem89@gmail.com>
2024-10-28 15:38:45 +0300

    V3

--- src/app/api/testdb/route.js
+++ src/app/api/testdb/route.js
@@ -1,6 +1,6 @@
 import { pool } from "DB/Postgres";
 
-export default async function handler(req, res) {
+export default async function GET(req, res) {
   try {
     // Connect to the database and run a simple query
     const result = await pool.query('SELECT NOW()');
